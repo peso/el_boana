5f908ad0d3bcf853f12e14eb7ba76ad518bda578
Peer Sommerlund <peso@users.sourceforge.net>
2020-03-12 19:46:02 +0100

    Read event loop while reading mouse position.
    This is a hack, to be able to close the application.

--- C/mouse.c
+++ C/mouse.c
@@ -4,18 +4,23 @@
 
 #include "mouse.h"
 
-#include <SDL_mouse.h>
+#include <SDL.h>
 
 void SetMouse( int_32 x, int_32 y )
 {
     /* Not impleented */
 }
-/*
+/* Blocking method! waits for next event for up to 1000 ms
   1  left button
   2  right button
 */
 void GetMouse( int_16* x , int_16* y, int_16* b )
 {
+    SDL_Event event;
+    SDL_WaitEventTimeout(&event, 1000);
+    
+    if (event.type == SDL_QUIT) exit(1);
+
     Uint32 button = SDL_GetMouseState(x,y);
     *b = 0 
     | button & SDL_BUTTON_LMASK ? MOUSE_LB : 0
